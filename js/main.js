b9d633a55b65822ea8be90d308d152c4f40c5257
Spaceman <alexp@jolera.com>
2015-07-13 00:03:41 -0400

    basic object functions

--- js/main.js
+++ js/main.js
@@ -5,7 +5,7 @@ var scene = new tr.Scene()
 var camera = new tr.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000)
 
 var renderer = new tr.WebGLRenderer()
-renderer.setSize (window.innerWidth, window.innerHeight)
+renderer.setSize(window.innerWidth, window.innerHeight)
 document.body.appendChild(renderer.domElement)
 
 
@@ -16,9 +16,69 @@ var defaultWFMaterial = {
                           transparent:true,
                           wireframe:true
                         }
-                        
 
-// CREATE GEOMETRY FUNCTIONS
+var defaultWFPlane    = {
+                          color:'rgb(200,200,200)',
+                          opacity:0.5,
+                          transparent:true,
+                          wireframe:true,
+                          side:tr.DoubleSide
+                        }
+
+
+// CREATE BASIC GEOMETRY FUNCTIONS
+function generateTriangle(vertices, material, objectName){
+  if(!vertices){
+    vertices = [[0,0,0],[2,3,0],[4,0,0]]
+  }
+  var geometry = new tr.Geometry()
+  geometry.vertices.push(new tr.Vector3(vertices[0][0],vertices[0][1],vertices[0][2]))
+  geometry.vertices.push(new tr.Vector3(vertices[1][0],vertices[1][1],vertices[1][2]))
+  geometry.vertices.push(new tr.Vector3(vertices[2][0],vertices[2][1],vertices[2][2]))
+  geometry.faces.push(new tr.Face3(0,1,2))
+
+  if(!material){
+    material = new tr.MeshBasicMaterial(defaultWFPlane)
+  }
+
+  var triangle = new tr.Mesh(geometry, material)
+
+  if(objectName){
+    triangle.name = objectName
+  }
+
+  scene.add(triangle)
+}
+
+function generateTriangle_new (options) {
+  if (!options) {
+    options = {
+      name: 'triangle' + (scene.children.length)
+    , vertices: [[0,0,0],[2,3,0],[4,0,0]]
+    , material: defaultWFPlane
+    }
+  }
+
+  var geometry = new tr.Geometry()
+  geometry.vertices.push(new tr.Vector3(options.vertices[0][0],options.vertices[0][1],options.vertices[0][2]))
+  geometry.vertices.push(new tr.Vector3(options.vertices[1][0],options.vertices[1][1],options.vertices[1][2]))
+  geometry.vertices.push(new tr.Vector3(options.vertices[2][0],options.vertices[2][1],options.vertices[2][2]))
+  geometry.faces.push(new tr.Face3(0,1,2))
+
+  var material = new tr.MeshBasicMaterial(options.material)
+
+  var triangle = new tr.Mesh(geometry, material)
+  triangle.name = options.name
+
+  scene.add(triangle)
+}
+generateTriangle_new()
+generateTriangle_new()
+generateTriangle_new()
+generateTriangle_new()
+generateTriangle_new()
+generateTriangle_new()
+
 function generateCube(geometry, material, objectName){
   if(!geometry){
     geometry = [1,1,1]
@@ -37,9 +97,53 @@ function generateCube(geometry, material, objectName){
   }
 
   scene.add(cube)
-
-  var objectGenerated = scene.children[scene.children.length-1]
 }
+
+function generatePlane(geometry, material, objectName){
+  if(!geometry){
+    geometry = [5,5]
+  }
+  geometry = new tr.PlaneGeometry(geometry[0],geometry[1])
+
+  if(!material){
+    material = defaultWFPlane
+  }
+  material = new tr.MeshBasicMaterial(material)
+
+  var plane = new tr.Mesh(geometry, material)
+
+  if(objectName){
+    plane.name = objectName
+  }
+
+  scene.add(plane)
+}
+
+function generateHex(objName, objSize, objColor, wireOn){
+  var geometry = new tr.Geometry();
+  geometry.vertices.push(
+    new THREE.Vector3(0, 0, 0),
+    new THREE.Vector3(2*objSize, 3*objSize, 0),
+    new THREE.Vector3(4*objSize, 0, 0),
+    new THREE.Vector3(2*objSize, -3*objSize, 0),
+    new THREE.Vector3(-2*objSize, -3*objSize, 0),
+    new THREE.Vector3(-4*objSize, 0, 0),
+    new THREE.Vector3(-2*objSize, 3*objSize, 0)
+  );
+  geometry.faces.push(
+    new THREE.Face3(0, 1, 2),
+    new THREE.Face3(0, 2, 3),
+    new THREE.Face3(0, 3, 4),
+    new THREE.Face3(0, 4, 5),
+    new THREE.Face3(0, 5, 6),
+    new THREE.Face3(0, 6, 1)
+  );
+  var material = new THREE.MeshBasicMaterial({color:objColor, wireframe:wireOn, side:THREE.DoubleSide});
+  var newGameObject = new THREE.Mesh(geometry, material);
+  newGameObject.name = objName;
+  scene.add(newGameObject);
+}
+// generateHex('hex1',1,'rgb(200,200,200)',true)
 
 
 // MODIFY GEOMETRY FUNCTIONS
@@ -52,21 +156,25 @@ function getObject(objectName){
 }
 
 
-// OBJECT/SCENE GENERATION
-generateCube([2,2,2],defaultWFMaterial,'cube1')
-generateCube()
+// // OBJECT/SCENE GENERATION
+// generateCube([2,2,2], defaultWFMaterial, 'cube1')
+// generateCube()
+// generatePlane([5,5], defaultWFPlane, 'plane1')
+// generatePlane()
 
 
-// OBJECT/SCENE MODIFICATION
-camera.position.z = 5
-findObject('cube1').position.y = 2
+// // OBJECT/SCENE MODIFICATION
+camera.position.z = 15
+// getObject('cube1').position.y = 2
+// getObject('plane1').position.y = -2
 
 
 // RENDER LOOP
 function render(){
   requestAnimationFrame(render)
 
-  getObject('cube1').rotation.x += 0.01
+  // getObject('cube1').rotation.x += 0.01
+  // getObject('plane1').rotation.y += 0.01
 
   renderer.render(scene, camera)
 }
