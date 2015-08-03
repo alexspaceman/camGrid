9a5d0767bc781347bc51e220f013ea80d26c59b4
alexspaceman <alexp@jolera.com>
2015-08-02 21:35:50 -0400

    express serving index.html, along with three.js and game.build.js

--- js/server/server.js
+++ js/server/server.js
@@ -1,33 +1,77 @@
 // GLOBALS
-let express = require('express')
+const express = require('express')
 
-let app = express()
-let port = process.env.PORT || 8080
+const app = express()
+
+const router = express.Router()
+const apiRouter = express.Router()
+
+const port = process.env.PORT || 8080
+
+let routerOptions = { root: __dirname }
 
 
 // MIDDLEWARE
-app.use((req, res, next) => {
+router.use((req, res, next) => {
   console.log(req.method, req.url)
+  next()
 })
 
 
 // ROUTES
-app.use(express.static('html'))
-// app.get('/', (req, res, next) => {
-//   let options = {
-//     root: __dirname + '/html/'
-//   }
+router.get('/', (req, res) => {
+  res.send(`<h1>Spaceman's Office</h1><h3>Sitemap</h3><h5><a href="http://localhost:${port}/about">About</a></h5>`)
+})
 
-//   res.sendFile('index.html', options, (err) => {
-//     if (err) {
-//       console.log(err)
-//       res.status(err.status).end()
-//     }
-//     else {
-//       console.log('sent index.html')
-//     }
-//   })
-// })
+router.get('/about', (req, res) => {
+   res.send(`<h1>About</h1><h5>This website will house some organizational tools and projects</h5><h5><a href="http://localhost:${port}/">Home</a></h5>`)  
+})
+
+apiRouter.get('/', (req, res) => {
+  res.send(`api page`)
+})
+
+// index
+router.get('/index', (req, res) => {
+  res.sendFile('html/index.html', routerOptions, (err) => {
+    if (err) {
+      console.log(err)
+      res.status(err.status).end()
+    }
+    else {
+      console.log('loaded index')
+    }
+  })
+})
+
+router.get('/js/utils/three.js', (req, res) => {
+  res.sendFile('js/utils/three.js', routerOptions, (err) => {
+    if (err) {
+      console.log(err)
+      res.status(err.status).end()
+    }
+    else {
+      console.log('loaded threejs')
+    }
+  })
+})
+
+router.get('/js/build/game.build.js', (req, res) => {
+  res.sendFile('js/build/game.build.js', routerOptions, (err) => {
+    if (err) {
+      console.log(err)
+      res.status(err.status).end()
+    }
+    else {
+      console.log('loaded game')
+    }
+  })
+})
+
+
+// USER ROUTER
+app.use('/', router)
+app.use('/api', apiRouter)
 
 
 // START THE SERVER
--- server.build.js
+++ server.build.js
@@ -4,30 +4,70 @@
 var express = require('express');
 
 var app = express();
+
+var router = express.Router();
+var apiRouter = express.Router();
+
 var port = process.env.PORT || 8080;
 
+var routerOptions = { root: __dirname };
+
 // MIDDLEWARE
-app.use(function (req, res, next) {
+router.use(function (req, res, next) {
   console.log(req.method, req.url);
+  next();
 });
 
 // ROUTES
-app.use(express['static']('html'));
-// app.get('/', (req, res, next) => {
-//   let options = {
-//     root: __dirname + '/html/'
-//   }
+router.get('/', function (req, res) {
+  res.send('<h1>Spaceman\'s Office</h1><h3>Sitemap</h3><h5><a href="http://localhost:' + port + '/about">About</a></h5>');
+});
 
-//   res.sendFile('index.html', options, (err) => {
-//     if (err) {
-//       console.log(err)
-//       res.status(err.status).end()
-//     }
-//     else {
-//       console.log('sent index.html')
-//     }
-//   })
-// })
+router.get('/about', function (req, res) {
+  res.send('<h1>About</h1><h5>This website will house some organizational tools and projects</h5><h5><a href="http://localhost:' + port + '/">Home</a></h5>');
+});
+
+apiRouter.get('/', function (req, res) {
+  res.send('api page');
+});
+
+// index
+router.get('/index', function (req, res) {
+  res.sendFile('html/index.html', routerOptions, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    } else {
+      console.log('loaded index');
+    }
+  });
+});
+
+router.get('/js/utils/three.js', function (req, res) {
+  res.sendFile('js/utils/three.js', routerOptions, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    } else {
+      console.log('loaded threejs');
+    }
+  });
+});
+
+router.get('/js/build/game.build.js', function (req, res) {
+  res.sendFile('js/build/game.build.js', routerOptions, function (err) {
+    if (err) {
+      console.log(err);
+      res.status(err.status).end();
+    } else {
+      console.log('loaded game');
+    }
+  });
+});
+
+// USER ROUTER
+app.use('/', router);
+app.use('/api', apiRouter);
 
 // START THE SERVER
 app.listen(port);
